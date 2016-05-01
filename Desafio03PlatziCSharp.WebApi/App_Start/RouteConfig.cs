db33d3787a7e8a1dbcb74b61af2d8aa420f2d1b5
Erick Orlando Velasco Martínez <evelasco@insideb2b.com>
2016-05-01 15:48:31 -0500

    Completado el CRUD del proyecto Web y traducido al Español

--- Desafio03PlatziCSharp.WebApi/App_Start/RouteConfig.cs
+++ Desafio03PlatziCSharp.WebApi/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Desafio03PlatziCSharp.WebApi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
