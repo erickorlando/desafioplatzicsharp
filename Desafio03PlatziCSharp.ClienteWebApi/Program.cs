d1274241d4e73301176ea786da03eccd37616f7a
Erick Orlando Velasco Martínez <evelasco@insideb2b.com>
2016-05-01 16:14:58 -0500

    Creacion de proyecto de consola como Cliente del Web API

---
+++ Desafio03PlatziCSharp.ClienteWebApi/Program.cs
@@ -0,0 +1,25 @@
+﻿// Usamos la característica de C# 6.0
+using static System.Console;
+
+namespace Desafio03PlatziCSharp.ClienteWebApi
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Encabezado("Desafío N° 03 del Curso Básico de C# en Platzi\nConectar al Web API");
+
+            //TODO: Terminar de conectar al Servicio.
+            ReadLine();
+        }
+
+        private static void Encabezado(string mensaje)
+        {
+            WriteLine("".PadRight(35, '-'));
+            WriteLine(mensaje);
+            WriteLine("".PadRight(35, '-'));
+        }
+    }
+}
