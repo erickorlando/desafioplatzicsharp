7f7578f1e8c0cac572b84ebc9293069d738599d9
Erick Orlando Velasco Martínez <evelasco@insideb2b.com>
2016-05-01 12:29:41 -0500

    Desafio 02 de #PlatziCSharp terminado

---
+++ Desafio02PlatziCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using static System.Console;
+
+namespace Desafio02PlatziCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Encabezado("Desafío 02: Lectura de Propiedades de un archivo BMP");
+            using (var archivoBmp = File.Open("./Images/PlatziCSharp.bmp", FileMode.Open))
+            {
+                using (var imagenBmp = new BinaryReader(archivoBmp))
+                {
+                    imagenBmp.BaseStream.Seek(18, SeekOrigin.Begin);
+                    WriteLine($"Ancho de la imagen: {imagenBmp.ReadInt32()}");
+
+                    imagenBmp.BaseStream.Seek(22, SeekOrigin.Begin);
+                    WriteLine($"Altura de la imagen: {imagenBmp.ReadInt32()}");
+
+                    imagenBmp.BaseStream.Seek(28, SeekOrigin.Begin);
+                    WriteLine($"Bits por Pixel: {imagenBmp.ReadInt32()}");
+                }
+            }
+
+            ReadLine();
+        }
+        private static void Encabezado(string mensaje)
+        {
+            WriteLine("".PadRight(35, '-'));
+            WriteLine(mensaje);
+            WriteLine("".PadRight(35, '-'));
+        }
+    }
+}
--- DesafioPlatziCSharp/Program.cs
+++ DesafioPlatziCSharp/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using DesafioPlatziCSharp;
 // Usamos la característica de C# 6.0
+using static System.Console;
 
 namespace Desafio01PlatziCSharp
 {
@@ -13,14 +12,14 @@ namespace Desafio01PlatziCSharp
 
             var listaProfesores = new List<Profesor>();
 
-            Encabezado("Leemos la lista de Profesores del archivo de Texto");
+            Encabezado("Desafío 01: Leemos la lista de Profesores del archivo de Texto");
             var lineasProfesores = File.ReadAllLines("./Files/Profesores.txt");
 
             int contador = 0;
             foreach (var linea in lineasProfesores)
             {
                 listaProfesores.Add(new Profesor { Id = contador++, Nombres = linea });
-                Console.WriteLine($"Profesor en posición {contador} es {linea}");
+                WriteLine($"Profesor en posición {contador} es {linea}");
             }
 
             string rutaArchivoBinario = "./Files/Profesores.bin";
@@ -37,7 +36,7 @@ namespace Desafio01PlatziCSharp
                     }
                 }
             }
-            Console.ReadLine();
+            ReadLine();
             Encabezado("Leeemos el archivo binario y lo imprimimos en pantalla");
 
             using (var archivoBinario = File.Open(rutaArchivoBinario, FileMode.Open))
@@ -47,20 +46,20 @@ namespace Desafio01PlatziCSharp
                     // Mientras se pueda leer.
                     while (binario.BaseStream.Position < binario.BaseStream.Length)
                     {
-                        Console.WriteLine($"Id:{binario.ReadInt32()}, Nombre:{binario.ReadString()}");
+                        WriteLine($"Id:{binario.ReadInt32()}, Nombre:{binario.ReadString()}");
                     }
                     Encabezado("Fin del archivo");
                 }
             }
 
-            Console.ReadLine();
+            ReadLine();
         }
 
         private static void Encabezado(string mensaje)
         {
-            Console.WriteLine("".PadRight(15, '-'));
-            Console.WriteLine(mensaje);
-            Console.WriteLine("".PadRight(15, '-'));
+            WriteLine("".PadRight(15, '-'));
+            WriteLine(mensaje);
+            WriteLine("".PadRight(15, '-'));
         }
     }
 }
